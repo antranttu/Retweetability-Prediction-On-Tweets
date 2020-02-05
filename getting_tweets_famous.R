fe22cf0f3722a6156461d7c72748b4a6983bea5c
atranttu <42917463+atranttu@users.noreply.github.com>
2020-02-05 00:50:00 -0800

    Update getting_tweets_famous.R

--- getting_tweets_famous.R
+++ getting_tweets_famous.R
@@ -18,10 +18,10 @@ library(rtweet)
 ## access token method: create token and save it as an environment variable
 create_token(
   app = "my_twitter_research_app",
-  consumer_key = "MO6yQEsEys0SJCb9pcQl5Bs8Z",
-  consumer_secret = "swyUqTpbjMX4dkJ1WCGVRuqqmCjewGjTpwEXHByy5SpNrsuir7",
-  access_token = "757085451555483648-OIc9CErvPN8yrrxRFffir4V7TFS5lt1",
-  access_secret = "G6JI4BYfgDVqT8U4WW8yKWq3JV8Fi0RbLwmafwN5u9ve3")
+  consumer_key = "X", # For security purposes, i'm not showing my keys here. Just replace the X's with your own keys
+  consumer_secret = "X",
+  access_token = "X",
+  access_secret = "X")
 
 ## lookup users by screen_name or user_id
 users <- c("KimKardashian", "justinbieber", "taylorswift13",
